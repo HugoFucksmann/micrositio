4ebade4c9afdee6752da0e6bab4d27b2347a845b
hfucksmann <hugoffuksmann@gmail.com>
2022-05-09 11:21:16 -0300

    gg

---
+++ src/pages/home/home.js
@@ -0,0 +1,11 @@
+import Layout from "../../layout/layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div>HOME WEB</div>
+    </Layout>
+  );
+};
+
+export default Home;
---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import * as ReactDOMClient from "react-dom/client";
+import "./home.css";
+import "../bootstrap";
+import Layout from "../layout/layout";
+
+const home = ReactDOM.createRoot(document.getElementById("home"));
+home.render(
+  <React.StrictMode>
+    <Layout>
+      <div>HOME WEB</div>
+    </Layout>
+  </React.StrictMode>
+);
